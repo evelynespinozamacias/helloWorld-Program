81e5ad1ca35f86accaa1d85e4baecb0ce75d1a8b
evelynespinozamacias <evelynespinozamacias@gmail.com>
2021-05-13 18:28:28 -0500

    hello world commit program

---
+++ helloWorld/src/helloWorld.java
@@ -0,0 +1,7 @@
+public class helloWorld {
+    public static void main(String[] args) {
+
+        //Print the words "hello world" on the screen
+        System.out.println(" Hello World!");
+    }
+}
